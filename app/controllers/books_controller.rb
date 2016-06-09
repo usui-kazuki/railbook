9f3da19d201008357b44400d3729f5cde1d6d5b8
usui kazuki <usui-kazuki@outlook.jp>
2016-06-09 09:16:05 +0900

    0

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -66,6 +66,9 @@ class BooksController < ApplicationController
     render :index
   end
 
+  def view_helper_test
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :books do
     get 'search', on: :collection
   end
+
+  resources :books do
+    get 'view_helper_test', on: :collection
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
