ea74943b8839ff391c1b34fd05a9d1580129fd38
usui kazuki <usui-kazuki@outlook.jp>
2016-07-11 18:36:08 +0900

    変更点B終了

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706012524) do
+ActiveRecord::Schema.define(version: 20160711075334) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 20160706012524) do
   create_table "reviews", force: :cascade do |t|
     t.string   "book_id"
     t.text     "body"
+    t.string   "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "userid"
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
