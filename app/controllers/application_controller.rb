7bfe3ebe644429c6934de578db8610e693e2d1f4
usui kazuki <usui-kazuki@outlook.jp>
2016-09-14 12:50:04 +0900

    ログイン機能の追加＿その２

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :check_logined
+  private
+  def check_logined
+    if session[:usr]
+      begin
+        @usr = User.find(session[:usr])
+      rescue ActiveRecord::RecordNotFound
+        reset_session
+      end
+    end
+
+    unless @usr
+      flash[:referer] = request.fullpath
+      redirect_to controller: :login,action: :index
+    end
+  end
 end
---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,18 @@
+﻿class LoginController < ApplicationController
+  skip_before_action :check_logined
+  def index
+  end
+
+  def auth
+    usr = User.authenticate(params[:userid],params[:password])
+    if usr then
+      reset_session
+      session[:usr] = usr.id
+      redirect_to params[:referer]
+    else
+      flash.now[:referer] = params[referer]
+      @error = 'ユーザーID/パスワードが間違っています。'
+      render 'index'
+    end
+  end
+end
